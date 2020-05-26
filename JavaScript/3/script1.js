bee7aa7b307f0281acbc08e1718a4a55480cac7e
OsipovAK <osipovandrey1986@gmail.com>
2020-05-26 16:04:04 +0700

    fix function

--- JavaScript/3/script1.js
+++ JavaScript/3/script1.js
@@ -1,24 +1,43 @@
-function random(number) {
-    return Math.ceil(Math.random() * number);
-}
-let number = random(1000);
-let answer = prompt('Угадай число от 1 до 1000');
-// var gameAgain = confirm("Сыграем еще?");
-if (answer == null) {
-    alert('игра заканчивается');
-    answer == + answer;
-} else {
-    while (answer != number && answer != null) {
-        if (answer < number) {
-            answer = prompt('Больше!');
-        } else if (answer > number) {
-            answer = prompt('Меньше!');
-        } else if (answer != parseInt(answer)) {
-            answer = prompt('Введи число!')
-        }
-    } if (answer == number) {
-        alert('Правильно!');
-    } else if (answer == null) {
-        alert('игра заканчивается');
+var number = random(1000);
+var s = 0;
+var gameAgain = 1;
+
+while (gameAgain > 0) {
+    do {
+        var answer = prompt('Введите число от 1 до 1000');
+        var s = checkAnswer(number, answer);
+    }
+    while (s < 4) {
+        var gameAgain = confirm("Сыграем еще?") ? 1 : 0;
+        var number = random(1000);
     }
 }
+
+function random(number) {
+    return Math.ceil(Math.random() * number);
+};
+function checkAnswer(number, answer) {
+    if (answer == null) {
+        alert('игра заканчивается');
+        s = 4;
+        return s;
+    } else if (answer != number && answer != null) {
+        if (answer < number) {
+            alert('Больше!');
+            s = 1;
+            return s;
+        } else if (answer > number) {
+            alert('Меньше!');
+            s = 2;
+            return s;
+        } else if (answer != parseInt(answer)) {
+            alert('Вы ввели не число. Введите число!');
+            s = 3;
+            return s;
+        }
+    } else {
+        alert('Правильно!');
+        s = 4;
+        return s;
+    }
+};

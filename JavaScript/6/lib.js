f8cd26def24a13e69f77d4ca2157dbfa0696c422
OsipovAK <osipovandrey1986@gmail.com>
2020-06-01 23:45:42 +0700

    done task in lesson 6

--- JavaScript/6/lib.js
+++ JavaScript/6/lib.js
@@ -1,98 +1,33 @@
-// alert(
-//     [1, 2, 3].join('\n')
-// )
-
 (function () {
-    var userText = '';
+    var name = '';
     var user = [];
-    var userObj = {};
     var arrUser = [];
+    var str = '';
+    var allStr = '';
     window.start = function start() {
         do {
-            userText = prompt('Введите имя и фамилию через пробел!');
-            if (userText != null) {
-                User(userText);
+            name = prompt('Введите имя и фамилию через пробел!');
+            if (name != null) {
+                name = name.split(' ');
+                user = new User(name[0], name[1]);
+                arrUser.push(user);
             }
         }
-        while (userText != null) {
+        while (name != null) {
             getAllUsers(user);
         }
-        function User(userText) {
-            arrUser = userText.split(' ');
-            userObj = {
-                name: arrUser[0],
-                lastName: arrUser[1],
-                regDate: printFullTime()
-            };
-            console.log(userObj);
-            user.push(userObj);
+        function User(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.regDate = new Date().toLocaleString();
         }
         function getAllUsers(user) {
-            var long = user.length;
-            var str = '';
-            var array = [];
-            var i = 0;
-            do {
-                for (var prop in user) {
-                    var propValue = user[prop];
-                    array.push(propValue);
-                    str = array.join(' ');
-                    console.log(str);
-                };
-                i++;
+            var long = arrUser.length;
+            for (let index = 0; index < arrUser.length; index++) {
+                str = arrUser[index].firstName + ' ' + arrUser[index].lastName + ' ' + arrUser[index].regDate + '\n';
+                allStr = str + allStr;
             }
-            while (i < long) {
-                console.log(long);
-                // alert('Текст\nТекст2.\n');
-            }
+            alert(allStr);
         }
-        function printFullTime() {
-            var d = new Date();
-            var month = getRusMonth(d);
-            var date = d.getDate();
-            var fullYear = d.getFullYear();
-            var h = d.getHours();
-            var min = d.getMinutes();
-            var s = d.getSeconds();
-            function getRusMonth(d) {
-                let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-                return months[d.getMonth()];
-            };
-            var textFormat = date + ' ' + month + ' ' + fullYear + ' г. ' + h + ':' + min + ':' + s;
-            return textFormat;
-        };
     }
 })()
-
-/* Написать простой прототип системы регистрации пользователей. Для этого
-используйте две функции-конструктора:
-
-1. User, которая создает объект типа «пользователь» со следующими свойствами:
-{ // имя firstName: ‘', // фамилия lastName: '’, // дата и время регистрации regDate: ''}.
-Объекты типа User предназначены для сохранения каждой отдельной записи о пользователе.
-
-2. UserList, которая создает объект типа “список пользователей” со следующими свойствами
-и методами: { // внутреннее свойство-массив, в которое будут попадать объекты типа
-User users: [], // метод получает в качестве параметра объект типа User и сохраняет
-его в массив users add: function(user) {}, // метод возвращает список пользователей из массива
-users getAllUsers: function() {}}. Объекты типа UserList предназначены для хранения множества
-записей типа User.
-
-При запуске программы должно происходить:
-
-1. Создавать новый экземпляр объекта UserList (с пустым свойством-массивом users).
-
-2. Открываться диалог prompt с предложением ввести имя и фамилию пользователя для
-регистрации (одной строкой через пробел).
-
-3. После нажатия ОК должен создаваться новый экземпляр объекта User с заполнением
-свойств firstName, lastName и regDate (текущая дата и время).
-
-4. Созданный экземпляр объекта должен сохраняться в свойство-массив users созданного
-ранее объекта типа UserList (при помощи метода add).
-
-5. Диалог prompt должен повторяться, пока пользователь не нажмет “Отмена”.
-
-6. После нажатия «Отмена» выведите на экран (в консоль или при помощи alert) список
-всех пользователей с именами и датами регистрации (используйте для этого метод getAllUsers).
-*/
